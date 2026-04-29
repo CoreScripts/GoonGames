2d1bc23dbe1ea939a200b09cab478d4603f6c73b
CoreScripts <corexploits@gmail.com>
2026-04-30 01:54:01 +0800

    Update games.js

--- games.js
+++ games.js
@@ -2,20 +2,20 @@ const GAMES_DATA = [
     {
         id: "0",
         title: "Ruffle Demo",
-        image: "https://f-droid.org/repo/rs.ruffle/en-US/icon_bQ_yOJtIW_1yIHpvXy6Y1s9nmS_2ZPXbqXO6yJna6b8=.png",
+        image: "https://sc.filehippo.net/images/t_app-icon-l/p/74ac306e-214e-40ee-963e-f1b8ee379f27/1693778373/ruffle-logo",
         url: "https://ruffle.rs/demo/",
         source: "https://github.com/ruffle-rs/ruffle"
     },
     {
         id: "1",
+        title: "2048",
+        image: "https://play-lh.googleusercontent.com/bZregCeEpzMwRoocLnsYb4Te-vPPHkW1k5H1xsQ3qK9yE00fixpWu5fNsUz3ut2IiDHn",
+        url: "https://play2048.co/"
+    },
+    {
+        id: "2",
         title: "Hill Climb Racing",
         image: "https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=314,height=314,fit=cover,f=auto/b87616bb88c0e3cb4c150c59fdc767fa/hill-climb-racing-lite.png",
         url: "games/hill-climb.html" 
     },
-    {
-        id: "2",
-        title: "2048",
-        image: "https://images.unsplash.com/photo-1614332287897-cdc485fa562d?w=400",
-        url: "https://play2048.co/"
-    }
 ];
