4628b3fd74b7d493d52478d258279367883e9597
CoreScripts <corexploits@gmail.com>
2026-04-30 01:46:23 +0800

    Update game entries in GAMES_DATA array

--- games.js
+++ games.js
@@ -1,16 +1,21 @@
 const GAMES_DATA = [
     {
-        id: "sample-game",
-        title: "Sample Retro Game",
-        image: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?w=400&h=250&fit=crop",
+        id: "0",
+        title: "Ruffle Demo",
+        image: "https://f-droid.org/packages/rs.ruffle/",
         url: "https://ruffle.rs/demo/",
         source: "https://github.com/ruffle-rs/ruffle"
     },
     {
-        id: "2048",
+        id: "1",
+        title: "Hill Climb Racing",
+        image: "https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=314,height=314,fit=cover,f=auto/b87616bb88c0e3cb4c150c59fdc767fa/hill-climb-racing-lite.png",
+        url: "games/hill-climb.html" 
+    },
+    {
+        id: "2",
         title: "2048",
-        image: "https://images.unsplash.com/photo-1614332287897-cdc485fa562d?w=400&h=250&fit=crop",
-        url: "https://play2048.co/",
-        source: "https://github.com/gabrielecirulli/2048"
+        image: "https://images.unsplash.com/photo-1614332287897-cdc485fa562d?w=400",
+        url: "https://play2048.co/"
     }
 ];
