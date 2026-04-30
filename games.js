cdfd632e32568b894d2ee8c3555191b63ba5d427
CoreScripts <corexploits@gmail.com>
2026-04-30 21:43:23 +0800

    Update game URLs to use loader.html

--- games.js
+++ games.js
@@ -1,3 +1,9 @@
+<!-- 
+    iframe: games/loader.html?url=[INSERT URL SITE]
+    swf: games/loader.html?url=games/assets/[INSERT FILE].swf
+-->
+
+
 const GAMES_DATA = [
     {
         id: "0",
@@ -16,12 +22,12 @@ const GAMES_DATA = [
         id: "2",
         title: "Hill Climb Racing",
         image: "https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=314,height=314,fit=cover,f=auto/b87616bb88c0e3cb4c150c59fdc767fa/hill-climb-racing-lite.png",
-        url: "games/hill-climb.html" 
+        url: "games/loader.html?url=hill-climb.html" 
     },
     {
         id: "3",
         title: "Poly Track",
         image: "https://uhighmidway.com/wp-content/uploads/2025/03/25-0331-PolyTrack.png",
-        url: "https://smapskajendfjshwbekf-prog.github.io/newcargame/" 
+        url: "games/loader.html?url=https://smapskajendfjshwbekf-prog.github.io/newcargame/" 
     }
 ];
